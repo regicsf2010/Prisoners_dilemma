2ef901be434dbfef9634efdca8eb8bf2f41a6013
Reginaldo <regi_csf@hotmail.com>
2014-04-08 16:38:05 -0300

    Commit inicial.

---
+++ Prisoners_dilemma/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 8, 2014
+ *      Author: reginaldo
+ */
+#include <iostream>
+using namespace std;
+#define EXIT 0;
+
+int main(void){
+
+	return EXIT;
+}
+
+
